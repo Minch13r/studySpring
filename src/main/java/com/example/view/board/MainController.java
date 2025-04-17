174f43d3d46f0d5f11c226e42b18d4f6e9220eca
minch13r <0414minyoung@naver.com>
2025-04-17 11:46:47 +0900

    main 기능

--- src/main/java/com/example/view/board/MainController.java
+++ src/main/java/com/example/view/board/MainController.java
@@ -1,17 +1,16 @@
-package com.example.view.member;
+package com.example.view.board;
 
 import com.example.biz.board.BoardService;
 import com.example.biz.board.BoardVO;
-import com.example.biz.board.impl.BoardDAO;
 import com.example.biz.member.MemberService;
 import com.example.biz.member.MemberVO;
-import com.example.biz.member.impl.MemberDAO;
-import com.example.view.controller.Controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
 @Component
 public class MainController implements Controller {
@@ -23,7 +22,7 @@ public class MainController implements Controller {
     private HttpSession session;
 
     @Override
-    public String execute(HttpServletRequest request, HttpServletResponse response) {
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("Main Controller executed");
 
         BoardVO boardVO = new BoardVO();
@@ -32,16 +31,19 @@ public class MainController implements Controller {
         memberVO.setMid(request.getParameter("mid"));
         memberVO.setMpw(request.getParameter("mpw"));
 
+        ModelAndView mav = new ModelAndView();
         // 상세페이지 보기
         boardVO.setBid(Integer.parseInt(request.getParameter("bid")));
         if(boardService.getBoard(boardVO) != null) {
             request.setAttribute("boardVO", boardVO);
             // 상세페이지 보기
-            return "detail";
+//            return "detail";
+            mav.setViewName("board");
         }
         // 안되면 메인 유지
         else {
-            return "main.do";
+//            return "main.do";
+            mav.setViewName("redirect:main.do");
         }
 
 
@@ -49,18 +51,14 @@ public class MainController implements Controller {
         if(memberService.getMember(memberVO) != null) {
             request.setAttribute("memberVO", memberVO);
             // 글 작성 페이지로 이동
-            return "insert";
+//            return "insert";
+            mav.setViewName("insert");
         }
         // 안되면 메인 유지
         else {
-            return "main.do";
+//            return "main.do";
+            mav.setViewName("redirect:main.do");
         }
-
-        // 로그아웃
-        // 세션 비우기
-        session.invalidate();
-        return "login";
-
-        // 페이지 이동은 있다고 치고.. 기능만....
+        return mav;
     }
 }
