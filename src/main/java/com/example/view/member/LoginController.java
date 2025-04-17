c609f149eae54111fe2f7db95c6254321ac52456
minch13r <0414minyoung@naver.com>
2025-04-17 10:46:21 +0900

    스프링 xml로 사용

--- src/main/java/com/example/view/member/LoginController.java
+++ src/main/java/com/example/view/member/LoginController.java
@@ -2,13 +2,13 @@ package com.example.view.member;
 
 import com.example.biz.member.MemberService;
 import com.example.biz.member.MemberVO;
-import com.example.biz.member.impl.MemberDAO;
-import com.example.view.controller.Controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
 @Component
 public class LoginController implements Controller {
@@ -18,7 +18,7 @@ public class LoginController implements Controller {
     private HttpSession session;
 
     @Override
-    public String execute(HttpServletRequest request, HttpServletResponse response) {
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("Login Controller executed");
 
         // DB 연동
@@ -30,14 +30,20 @@ public class LoginController implements Controller {
 
         memberVO.setMid(request.getParameter("mid"));
         memberVO.setMpw(request.getParameter("mpw"));
+
         // 페이지 이동 처리
+        ModelAndView mav = new ModelAndView();
+        // 로그인 성공시 메인페이지 이동
         if(memberService.getMember(memberVO) != null) {
-            return "main.do";
+//            return "main.do";
+            mav.setViewName("redirect:main.do"); // 아무설정도 안 하면 forward 형식으로 함
+            // redirect 하고 싶으면 앞에 명시해줘야 함
         }
+        // 로그인 실패시 로그인페이지 유지
         else {
-            return "login"; // .jsp 를 자동으로 붙여줌
+//            return "login"; // .jsp 를 자동으로 붙여줌
+            mav.setViewName("login");
         }
-
+        return mav;
     }
-
 }
