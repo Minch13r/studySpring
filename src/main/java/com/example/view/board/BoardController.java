52840932ce3662b61c3721e0fa8218c0a7fed0ef
minch13r <0414minyoung@naver.com>
2025-04-17 11:46:59 +0900

    글 확인 기능

--- src/main/java/com/example/view/board/BoardController.java
+++ src/main/java/com/example/view/board/BoardController.java
@@ -1,30 +1,35 @@
-package com.example.view.member;
+package com.example.view.board;
 
 import com.example.biz.board.BoardService;
 import com.example.biz.board.BoardVO;
-import com.example.view.controller.Controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
 @Component
-public class DetailController implements Controller {
+public class BoardController implements Controller {
     @Autowired
     private BoardService boardService;
 
     @Override
-    public String execute(HttpServletRequest request, HttpServletResponse response) {
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("Detail Controller executed");
 
         BoardVO boardVO = new BoardVO();
         boardVO.setBid(Integer.parseInt(request.getParameter("bid")));
+        ModelAndView mav = new ModelAndView();
         if(boardService.getBoard(boardVO) != null){
             request.setAttribute("boardVO", boardVO);
-            return "detail.do";
+//            return "detail.do";
+            mav.setViewName("redirect:detail.do");
         }
         else {
-            return "main.do";
+//            return "main.do";
+            mav.setViewName("redirect:main.do");
         }
+        return mav;
     }
 }
