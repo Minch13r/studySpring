1cf7860b72b298a2a750ccd568c8ec9c32c06ba1
minch13r <0414minyoung@naver.com>
2025-04-24 10:31:53 +0900

    xml -> annotation으로 갈아타기

--- src/main/java/com/example/biz/common/CommonAdvice.java
+++ src/main/java/com/example/biz/common/CommonAdvice.java
@@ -4,10 +4,19 @@ import com.example.biz.board.BoardVO;
 import com.example.biz.member.MemberVO;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
 import org.springframework.util.StopWatch;
 
+// service단에서 실행되는 메서드들이라서 @Service 사용
+@Service
 public class CommonAdvice {
+
     // Before Advice - 메소드 실행 전에 동작
+    @Before("bPointcut()")
     public void beforeLog(JoinPoint jp) {
         System.out.println("================================");
         System.out.println("BEFORE 공통 로그");
@@ -47,16 +56,18 @@ public class CommonAdvice {
     }
 
     // AfterReturning Advice - 메소드가 정상적으로 반환된 후 동작
-    public void afterReturningLog(JoinPoint jp, Object returnObj) {
+    // 괄호 안의 인자가 두개 이상이면 그때부터는 return 값 넣어줘야 함.
+    @AfterReturning(pointcut = "aPointcut()", returning = "returnObj")
+    public void afterReturningLog(JoinPoint jp, Object returnObj) throws Exception {
         System.out.println("================================");
         System.out.println("RETURNING 공통 ");
 
         if (returnObj instanceof MemberVO) {
             System.out.println("RETURNING 공통 로그");
 
-            MemberVO vo = (MemberVO) returnObj;
+            MemberVO memberVO = (MemberVO) returnObj;
 
-            if (vo.getMrole().equals("ADMIN")) {
+            if (memberVO.getMrole().equals("ADMIN")) {
                 System.out.print("ADMIN ");
             }
             else {
@@ -69,7 +80,7 @@ public class CommonAdvice {
     }
 
     // AfterThrowing Advice - 메소드에서 예외가 발생했을 때 동작
-    public void afterThrowingLog(JoinPoint jp, Exception exceptObj) {
+    public void afterThrowingLog(JoinPoint jp, Exception exceptObj) throws Exception {
         System.out.println("================================");
         System.out.println("THROWING 공통 로그");
 
