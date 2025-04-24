0168896892b13566b05002c48e9917c41ddd1753
minch13r <0414minyoung@naver.com>
2025-04-24 10:31:39 +0900

    Pointcut 따로 관리하기

---
+++ src/main/java/com/example/biz/common/PointcutCommon.java
@@ -0,0 +1,23 @@
+package com.example.biz.common;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+// PoincutCommon은 메서드를 .aPointcut() 이런식으로 쓰지 않기에 new 안 해도 됨
+// @Component 이런거 안 써도 됨.
+public class PointcutCommon {
+    // 결합도를 낮추고 응집도를 높임
+    // 반복되는 코드를 줄였으니 결합도를 낮춤
+    // 관련있는 코드들끼리 묶어봤기에 응집도를 높임
+
+    // aPointcut 만들기(get만 허용)
+    @Pointcut("execution(* com.example.biz..*Impl.get*(..))")
+    public void aPointcut() {
+    }
+
+    // bPointcut 만들기(모든 메서드 허용)
+    @Pointcut("execution(* com.example.biz..*Impl.*(..))")
+    public void bPointcut() {
+    }
+}
